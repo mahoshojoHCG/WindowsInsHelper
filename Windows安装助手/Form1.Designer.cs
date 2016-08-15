3de11930bbc2df73ac066fd70ab82bc53155fdad
SwiftThera <yzy688@gmail.com>
2016-08-15 21:24:10 +0800

    第一版
    第一版，只是个渣渣，但还是要一本正经地介绍一下：
    没错，这是一个Windows硬盘安装器，但是却是用VHD启动的。
    
    用VHD启动安装Windows，就是把Windows安装文件解压到VHD文件里，然后启动。比起传统解压方式，有以下好处：
    
    1.防止混淆启动文件
    
    用传统解压方式安装，Windows安装程序有可能会把解压安装文件地分区标记为活动分区，不利于删除安装文件。
    
    2.文件方便删除
    
    只需删除一个VHD文件和启动项就可以移除安装项，传统方式需要删除许多文件且易误删系统文件。
    
    3.支持EFI启动
    
    ……
    
    支持系统：
    
    Windows 7、Windows 8、Windows 8.1、Windows 10。
    
    Windows Sever 2008 R2 、Windows Sever 2012、Windows Sever 2012 R2。
    
    64位，32位
    
    需要环境：.NET Framework 4.5.2

---
+++ Windows安装助手/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Windows安装助手
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(214)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(594, 83);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows 安装助手";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("等线", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(26, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "请选择您要安装的系统镜像文件";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("等线", 15.75F);
+            this.button1.Location = new System.Drawing.Point(521, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("等线", 13F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(23, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Windows镜像文件：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("等线", 13F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(23, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "安装文件存放位置：";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("等线", 15.75F);
+            this.button2.Location = new System.Drawing.Point(521, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("等线", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(26, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(489, 29);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "请选择您安装文件的存放位置";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Windows光盘镜像文件|*.iso";
+            this.openFileDialog1.Title = "选择Windows光盘镜像文件";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "WIH.vhdx";
+            this.saveFileDialog1.Filter = "Windows虚拟磁盘文件（Windows8及以上）|*.vhdx|Windows虚拟磁盘文件（Windows7）|*.vhd";
+            this.saveFileDialog1.Title = "选择Windows安装文件存放位置";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(27, 217);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(579, 27);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("等线", 15.75F);
+            this.button3.Location = new System.Drawing.Point(521, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "执行";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(646, 295);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Windows安装助手";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Windows安装助手/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Windows安装助手
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static bool efi;
+        public static string ApplicationPath;
+        public static bool finished = false;
+        public bool RedirectStandardError { get; set; }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //通过bceedit检查系统是否通过uefi固件启动
+            Process ifefi = new Process(); 
+            ifefi.StartInfo.FileName = "cmd.exe";
+            ifefi.StartInfo.Arguments = "/c bcdedit /enum {current}";
+            ifefi.StartInfo.UseShellExecute = false;
+            ifefi.StartInfo.RedirectStandardInput = true;
+            ifefi.StartInfo.RedirectStandardOutput = true;
+            ifefi.StartInfo.RedirectStandardError = true;
+            ifefi.StartInfo.CreateNoWindow = true;
+            ifefi.Start();
+            string  efiresult = ifefi.StandardOutput.ReadToEnd().ToString();
+            if(efiresult.Contains(@"\Windows\system32\winload.efi")) //判断bcdedit输出结果中是否含有“\Windows\system32\winload.efi”，如果有，系统就是通过uefi固件启动的，使用uefi模式。
+            {
+                //结果为uefi固件启动
+                //
+                //后期要加的东西，不要在意。(*^_^*)
+                //MessageBox.Show("您的电脑使用的是EFI启动模式，某些功能可能不可用。","提示",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+                //
+                efi = true;
+                this.Text = "Windows安装助手 EFI模式";
+            }
+            else //结果为bios固件启动
+            {
+                efi = false;
+                this.Text = "Windows安装助手 BIOS模式";
+            }
+            ApplicationPath = Application.StartupPath; //获取程序所在目录
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            textBox2.Text = saveFileDialog1.FileName;
+            if (saveFileDialog1.FileName.Contains("C:\\")) //判断是否用户准备在当前系统盘防止安装文件，如果用户准备在系统盘防止安装文件，提示用户可能无法全新安装。
+            {
+                MessageBox.Show("注意：\n您选择了在系统盘内保存安装文件，您在安装过程中将无法格式化系统盘！\n如果您在用户文件夹或者系统文件夹内保存，安装到当前系统盘的安装将完全无法进行！\n如果你完全看不懂这段话，请换个位置保存！","提示",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!finished) //判断是否完成，如果完成，按钮显示的时“重新启动计算机”，点击后程序将重启电脑
+            {
+                progressBar1.Value = 0; //重设processbar1进度
+                if ((textBox1.Text == "请选择您要安装的系统镜像文件") || (textBox1.Text == "")) //判断用户是否选择了文件
+                {
+                    MessageBox.Show("您尚未选择您要安装的系统镜像文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                else if ((textBox2.Text == "请选择您安装文件的存放位置") || (textBox2.Text == "")) //判断用户是否选择了文件
+                {
+                    MessageBox.Show("您尚未选择您要安装的系统镜像文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                else if ((!System.IO.File.Exists(ApplicationPath + "/bin/x64/7z.exe")) || (!System.IO.File.Exists(ApplicationPath + "/bin/x64/7z.dll"))) //检查7z组件是否缺失
+                {
+                    MessageBox.Show("程序目录中缺少了某些必须文件，请重新下载程序或者把程序解压到一个目录里而不是直接在压缩软件里双击运行！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    button3.Enabled = false;//禁用按钮控件，防止用户乱动
+                    progressBar1.Value = 10; //进度条滚动一些，安慰用户
+                    Directory.CreateDirectory(ApplicationPath + "/temp"); //创建临时文件夹
+                                                                          //下面那几句：创建diskpart脚本文件
+                    string[] DpShell = new string[] { @"Creat Vdisk File=""" + textBox2.Text + @""" Maximum=5000 Type=Expandable NOERR", @"Select Vdisk File=""" + textBox2.Text + @""" NOERR", "Attach Vdisk NOERR", "Creat Partition Primary", "Format FS=FAT32 Quick", "ASSIGN LETTER=Y", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", "Exit", };
+                    File.WriteAllLines(ApplicationPath + "/temp/cre.dp", DpShell);
+                    //下面几句：创建创建启动项时的脚本
+                    string Gudi = "{" + Guid.NewGuid().ToString() + "}";
+                    string[] BcdShell;
+                    if (efi) //efi与bios要区别对待
+                    {
+                        BcdShell = new string[] {  @"bcdedit /create " + Gudi + @" /d ""安装Windows"" /application osloader", @"bcdedit /set " + Gudi + @" device ramdisk =[Y:]\sources\boot.wim", @"bcdedit /set " + Gudi + @" path \windows\system32\winload.efi", @"bcdedit  /set " + Gudi + @" osdevice ramdisk=[Y:]\sources\boot.wim", @"bcdedit /set " + Gudi + @" systemroot \windows", @"bcdedit /set " + Gudi + @" winpe Yes", @"bcdedit /set " + Gudi + @" detecthal Yes", @"bcdedit /set " + Gudi + @" inherit {bootloadersettings}", @"bcdedit /set " + Gudi + @" locale zh-CN", @"bcdedit /set " + Gudi + @" ems Yes", @"bcdedit /displayorder " + Gudi + @" /addlast" };
+                    }
+                    else
+                    {
+                        BcdShell = new string[] {  @"bcdedit /create " + Gudi + @" /d ""安装Windows"" /application osloader", @"bcdedit  /set " + Gudi + @" device ramdisk =[Y:]\sources\boot.wim", @"bcdedit /set " + Gudi + @" path \windows\system32\winload.exe", @"bcdedit /set " + Gudi + @" osdevice ramdisk=[Y:]\sources\boot.wim", @"bcdedit /set " + Gudi + @" systemroot \windows", @"bcdedit /set " + Gudi + @" winpe Yes", @"bcdedit /set " + Gudi + @" detecthal Yes", @"bcdedit /set " + Gudi + @" inherit {bootloadersettings}", @"bcdedit /set " + Gudi + @" locale zh-CN", @"bcdedit /set " + Gudi + @" ems Yes", @"bcdedit /displayorder " + Gudi + @" /addlast" };
+                    }
+                    File.WriteAllLines(ApplicationPath + "/temp/newboot.cmd", BcdShell);
+                    //运行创建虚拟磁盘的diskpart脚本
+                    Process creatvdisk = new Process();
+                    creatvdisk.StartInfo.FileName = "diskpart.exe";
+                    creatvdisk.StartInfo.Arguments = @"/s """ + ApplicationPath + @"\temp\cre.dp""";
+                    creatvdisk.StartInfo.UseShellExecute = false;
+                    creatvdisk.StartInfo.RedirectStandardInput = true;
+                    creatvdisk.StartInfo.RedirectStandardOutput = true;
+                    creatvdisk.StartInfo.RedirectStandardError = true;
+                    creatvdisk.StartInfo.CreateNoWindow = true;
+                    creatvdisk.Start();
+                    creatvdisk.WaitForExit();
+                    progressBar1.Value = 20;
+                    //进度条滚动一些，安慰用户
+                    //处理虚拟硬盘结束
+                    //以下：利用7-zip将ISO文件解压到虚拟磁盘里
+                    Process unzip = new Process();
+                    unzip.StartInfo.FileName = ApplicationPath + @"\bin\x64\7z.exe";
+                    unzip.StartInfo.Arguments = @"x -y """ + openFileDialog1.FileName + @""" -oY:\";
+                    unzip.StartInfo.UseShellExecute = false;
+                    unzip.StartInfo.RedirectStandardInput = true;
+                    unzip.StartInfo.RedirectStandardOutput = true;
+                    unzip.StartInfo.RedirectStandardError = true;
+                    unzip.StartInfo.CreateNoWindow = true;
+                    unzip.Start();
+                    unzip.WaitForExit();
+                    progressBar1.Value = 80; //进度条滚动一些，安慰用户
+                                             //解压结束
+                                             //以下：添加启动项
+                    Process bcd = new Process();
+                    bcd.StartInfo.FileName = "cmd.exe";
+                    bcd.StartInfo.Arguments = "/c " + ApplicationPath + @"\temp\newboot.cmd";
+                    bcd.StartInfo.UseShellExecute = false;
+                    bcd.StartInfo.RedirectStandardInput = true;
+                    bcd.StartInfo.RedirectStandardOutput = true;
+                    bcd.StartInfo.RedirectStandardError = true;
+                    bcd.StartInfo.CreateNoWindow = true;
+                    bcd.Start();
+                    bcd.WaitForExit();
+                    //添加启动项结束
+                    progressBar1.Value = 100;
+                    if (RedirectStandardError) //检索错误信息
+                    {
+                        MessageBox.Show("发现如下错误：" + creatvdisk.StandardError.ReadToEnd() + unzip.StandardError.ReadToEnd() + bcd.StandardError.ReadToEnd(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        progressBar1.Value = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("操作成功完成", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                    //清理临时目录    
+                    Process cleandir = new Process();
+                    cleandir.StartInfo.UseShellExecute = false;
+                    cleandir.StartInfo.FileName = "cmd.exe";
+                    cleandir.StartInfo.Arguments = @"/c del /f /q """ + ApplicationPath + @"\temp""";
+                    cleandir.StartInfo.CreateNoWindow = true;
+                    cleandir.Start();
+                    cleandir.WaitForExit();
+                    Directory.Delete(ApplicationPath + "/temp");
+                    finished = true; //标记已处理完成
+                    button3.Enabled = true;
+                    button3.Text = "重新启动计算机";
+                }
+            }
+            else
+            {
+                //重启电脑的参数
+                Process restart = new Process();
+                restart.StartInfo.UseShellExecute = false;
+                restart.StartInfo.FileName = "shutdown.exe";
+                restart.StartInfo.Arguments = @"/r /f /t 0";
+                restart.Start();
+            }
+        }
+    }
+}
